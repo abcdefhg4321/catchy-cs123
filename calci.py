3cca6b121fbab6124780f04f04c49e855f236370
Ariix333 <55421311+Ariix333@users.noreply.github.com>
2019-09-17 09:41:15 +0530

    Update calci.py
    Added Sin,Cos,Tan functions

--- calci.py
+++ calci.py
@@ -1,5 +1,9 @@
-# Program make a simple calculator that can add, subtract, multiply and divide using functions
+# Last Edited by Ariix333
+# Program make a simple calculator that can add, subtract, multiply and divide using functions. (additionally sin,cos,tan functions)
 # This function adds two numbers 
+
+import math
+
 def add(x, y):
    return x + y
 # This function subtracts two numbers 
@@ -11,12 +15,21 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
    return x / y
+def sin(x):
+   return math.sin(x)
+def cos(x):
+   return math.cos(x)
+def tan(x):
+   return math.tan(x)
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.Exit")
+print("5.Sin")
+print("6.Cos")
+print("7.Tan")
+print("8.Exit")
 # Take input from the user 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -32,6 +45,12 @@ while i==1:
     elif choice == '4':
         print(num1,"/",num2,"=", divide(num1,num2))
     elif choice == '5':
+        print("sin(",num1,")=", sin(num1))
+    elif choice == '6':
+        print("cos",num1,"=", cos(num1))
+    elif choice == '7':
+        print("tan",num1,"=", tan(num1))
+    elif choice == '8':
         print("Exit. Good Bye")
         i=0
     else:    
